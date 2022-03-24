997e2991592ddb02626d843bcc83fe07b59098b4
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-03-24 09:17:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+	
+ TOKEN=os.environ.get("BOT_TOKEN",None)
+ SOURCE="https://github.com/iamgojoof6eyes/Source_Hider"
+ START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n @iamgojoof6eyes"
+ HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
+	
